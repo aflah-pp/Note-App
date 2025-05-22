500df8b4556be04e199874bf8812dde378a11683
Muhammed Aflah PP <nexusxrd@gmail.com>
2025-05-23 03:34:29 +0530

    Update views.py

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import View
+from django.http import FileResponse
+import os
 
-class FrontendAppView(TemplateView):
-    template_name = "index.html"
+class FrontendAppView(View):
+    def get(self, request):
+        file_path = os.path.join(settings.BASE_DIR, 'backend', 'staticfiles', 'index.html')
+        return FileResponse(open(file_path, 'rb'))
