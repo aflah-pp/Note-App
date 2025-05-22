fd47ae7f82c307a1d6fd47b63fa404d2d4acac2e
Muhammed Aflah PP <nexusxrd@gmail.com>
2025-05-23 03:26:39 +0530

    Update views.py

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,9 +1,4 @@
 from django.views.generic import TemplateView
-import os
 
 class FrontendAppView(TemplateView):
     template_name = "index.html"
-
-    def get_template_names(self):
-        # Serve the React index.html from frontend/dist
-        return [os.path.join('frontend', 'dist', 'index.html')]
